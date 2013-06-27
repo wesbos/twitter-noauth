a425bc9a8962acb46379db6e16477120073de9ec
wesbos <wesbos@gmail.com>
2013-06-27 10:48:09 -0400

    FIX YO 1.0

--- config.js
+++
@@ -1,4 +0,0 @@
-exports.CONSUMER_KEY = "il5vC7g9dY6uOgEFqD62Yg";
-exports.CONSUMER_SECRET = "hB5sFqiSggBDtHIE1rLaK7bhflh0HEdwi977xbNE6U";
-exports.ACCESS_TOKEN = "815246-gqewySRnlZzbko0Dpktgce8tvSqvFfW61K4dZsJFlM";
-exports.ACCESS_SECRET = "7J3XiKehPtQGJ9zURjdwkXhsEpSAfN7A4koEV5GsZg";
